467da8f116302d2157e28c4618712b972263b139
Mahesh Balasaheb Phatangare <63532168+Maheshphatangare@users.noreply.github.com>
2026-05-27 06:34:30 +0000

    new file

---
+++ scr/main/java/com/Client.java
@@ -0,0 +1,55 @@
+package com.yourapp.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private FinancialAdvisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client() {}
+
+    public Client(Long clientId, String firstName, String lastName, String email, String phone, FinancialAdvisor advisor) {
+        this.clientId = clientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+    }
+
+    // Getters & Setters
+    public Long getClientId() { return clientId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public FinancialAdvisor getAdvisor() { return advisor; }
+    public void setAdvisor(FinancialAdvisor advisor) { this.advisor = advisor; }
+
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
